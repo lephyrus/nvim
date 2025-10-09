91292cbebe0e16b42e09b5bdf4129c524384dc81
Lukas Rieder <1394536+lephyrus@users.noreply.github.com>
2025-10-09 11:44:30 +0200

    Sort buffer picker by lastused
    ... and disable preview, because that updates last used, unfortunately.
    Also remove the current buffer from the list.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,5 +1,19 @@
 return {
   "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      sources = {
+        buffers = {
+          current = false,
+          sort_lastused = true,
+          layout = {
+            preview = false,
+          },
+        },
+      },
+    },
+  },
   keys = {
     {
       "<leader>f.",
