30efaa690d6135f7b3c69e65c7f57f5d9f3ef098
Lukas Rieder <1394536+lephyrus@users.noreply.github.com>
2025-10-10 21:49:43 +0200

    Enable treesitter and prettier for EJS

---
+++ filetype.lua
@@ -0,0 +1,4 @@
+vim.filetype.add({ extension = { ejs = "ejs" } })
+vim.treesitter.language.register("html", "ejs")
+vim.treesitter.language.register("javascript", "ejs")
+vim.treesitter.language.register("embedded_template", "ejs")
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      ejs = { "prettierd", "prettier", stop_after_first = true },
+    },
+  },
+}
